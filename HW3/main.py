d92a73183562bae04b67c48f8388d8130b00f575
JFZzzzzz9048 <jocelynzhang9630@gmail.com>
2022-10-05 22:53:20 -0700

    HW3 Ready to submit
    Complete Transformer ~~

--- HW3/main.py
+++ HW3/main.py
@@ -1,13 +1,54 @@
 import sys
 
+from pyspark.sql import SparkSession
 from transformer import RollingAVGTransform
 
 
 def main():
-    # Transform a master DF into a DF of rolling averages.
-    rolling = RollingAVGTransform()
-    rolling_avg = rolling._transform()
-    rolling_avg.show(100)
+    # Setup Spark
+    database = "baseball"
+    username = "root"
+    pw = "password"
+    server = "localhost"
+    port = 3306
+    jdbc_url = f"jdbc:mysql://{server}:{port}/{database}?permitMysqlScheme"
+    jdbc_driver = "org.mariadb.jdbc.Driver"
+
+    # SQL query to generate a batter info dataframe
+    sql_batter_info = """
+    SELECT
+    BC.batter AS Batter_ID,
+    SUM(BC.atBat) AS atBat,
+    SUM(BC.Hit) AS Hit,
+    G.game_ID AS game_ID,
+    DATE(G.local_date) AS Game_Date
+    FROM batter_counts BC
+    JOIN game G
+    ON G.game_id = BC.game_id
+    GROUP BY BC.batter, DATE(G.local_date)
+    """
+
+    spark = SparkSession.builder.master("local[*]").getOrCreate()
+    df_batter_info = (
+        spark.read.format("jdbc")
+        .options(
+            url=jdbc_url,
+            query=sql_batter_info,
+            user=username,
+            password=pw,
+            driver=jdbc_driver,
+        )
+        .load()
+    )
+
+    # Generate a rolling average dataframe through transformer
+    rolling = RollingAVGTransform(
+        inputCols=["Batter_ID", "Game_Date", "atBat", "Hit"],
+        outputCol="Rolling_Average",
+    )
+    rolling_avg = rolling._transform(df_batter_info)
+    print("The first 30 rows of the 100 days rolling average table: ")
+    rolling_avg.show(30)
     return
 
 
--- HW3/transformer.py
+++ HW3/transformer.py
@@ -1,67 +1,51 @@
 from pyspark import keyword_only
 from pyspark.ml import Transformer
-from pyspark.sql import SparkSession
+from pyspark.ml.param.shared import HasInputCols, HasOutputCol
+from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
 from pyspark.sql import functions as func
 from pyspark.sql.functions import col
 from pyspark.sql.window import Window
 
-database = "baseball"
-username = "root"
-pw = "password"
-server = "localhost"
-port = 3306
-jdbc_url = f"jdbc:mysql://{server}:{port}/{database}?permitMysqlScheme"
-jdbc_driver = "org.mariadb.jdbc.Driver"
-sql_batter_info = """
-SELECT
-BC.batter AS Batter_ID,
-SUM(BC.atBat) AS atBat,
-SUM(BC.Hit) AS Hit,
-G.game_ID AS game_ID,
-DATE(G.local_date) AS Game_Date
-FROM batter_counts BC
-JOIN game G
-ON G.game_id = BC.game_id
-GROUP BY BC.batter, DATE(G.local_date)
-"""
 
-
-class RollingAVGTransform(Transformer):
+class RollingAVGTransform(
+    Transformer,
+    HasInputCols,
+    HasOutputCol,
+    DefaultParamsReadable,
+    DefaultParamsWritable,
+):
     @keyword_only
-    def __init__(self):
+    def __init__(self, inputCols=None, outputCol=None):
         super(RollingAVGTransform, self).__init__()
         kwargs = self._input_kwargs
         self.setParams(**kwargs)
+        return
 
     @keyword_only
-    def setParams(self):
+    def setParams(self, inputCols=None, outputCol=None):
         kwargs = self._input_kwargs
         return self._set(**kwargs)
 
-    def _transform(self):
-        spark = SparkSession.builder.master("local[*]").getOrCreate()
-        df_batter_info = (
-            spark.read.format("jdbc")
-            .options(
-                url=jdbc_url,
-                query=sql_batter_info,
-                user=username,
-                password=pw,
-                driver=jdbc_driver,
-            )
-            .load()
-        )
+    def _transform(self, dataset):
+        input_cols = self.getInputCols()
+        output_col = self.getOutputCol()
 
         w = (
             Window()
-            .partitionBy(df_batter_info["Batter_ID"])
-            .orderBy(df_batter_info["Game_Date"].cast("timestamp").cast("long"))
+            .partitionBy(dataset[input_cols[0]])
+            .orderBy(dataset[input_cols[1]].cast("timestamp").cast("long"))
             .rangeBetween(-8640000, -1)
         )
 
-        df = df_batter_info.withColumn("SUM_of_atBat", func.sum("atBat").over(w))
-        df = df.withColumn("SUM_of_Hit", func.sum("Hit").over(w))
-        df = df.withColumn("Rolling_Average", col("SUM_of_Hit") / col("SUM_of_atBat"))
-        df = df.sort("Batter_ID", "Game_Date").na.drop()
+        dataset = dataset.withColumn(
+            "SUM_of_atBat", func.sum(dataset[input_cols[2]]).over(w)
+        )
+        dataset = dataset.withColumn(
+            "SUM_of_Hit", func.sum(dataset[input_cols[3]]).over(w)
+        )
+        dataset = dataset.withColumn(
+            output_col, col("SUM_of_Hit") / col("SUM_of_atBat")
+        )
+        dataset = dataset.sort("Batter_ID", "Game_Date").na.drop()
 
-        return df
+        return dataset
